e1ff45f8cc7a5514594857a2d5f30c7d8b4f6145
Lucas <lucaszago.ph@gmail.com>
2022-08-25 12:49:17 +0200

    TCW complete Game.js without UI and _TryAgain()

--- Game.js
+++ Game.js
@@ -1,16 +1,61 @@
 class Game {
-  constructor(player1, player2) {
-    this._player1 = player1;
-    this._player2 = player2;
-    this._Init();
+  constructor() {
+    this._player1 = null;
+    this._player2 = null;
   }
 
-  _Init() {
+  _Init(player1, player2) {
     console.log("_Init Game");
+    this._player1 = player1;
+    this._player2 = player2;
   }
 
   _CheckResults() {
     console.log("Check Results");
+    this._player2._RandomizeSign();
+    if (this._player1._sign === "rock") {
+      switch (this._player2._sign) {
+        case "rock":
+          this._ShowResult("tie");
+          break;
+        case "paper":
+          this._ShowResult("lose");
+          this._player2._PointIncrease();
+          break;
+        case "scissors":
+          this._ShowResult("win");
+          this._player1._PointIncrease();
+          break;
+      }
+    } else if (this._player1._sign === "paper") {
+      switch (this._player2._sign) {
+        case "rock":
+          this._ShowResult("win");
+          this._player1._PointIncrease();
+          break;
+        case "paper":
+          this._ShowResult("tie");
+          break;
+        case "scissors":
+          this._ShowResult("lose");
+          this._player2._PointIncrease();
+          break;
+      }
+    } else {
+      switch (this._player2._sign) {
+        case "rock":
+          this._ShowResult("lose");
+          this._player2._PointIncrease();
+          break;
+        case "paper":
+          this._ShowResult("win");
+          this._player1._PointIncrease();
+          break;
+        case "scissors":
+          this._ShowResult("tie");
+          break;
+      }
+    }
   }
 
   _Reset() {
@@ -20,7 +65,21 @@ class Game {
     this._player1._sign = "rock";
     this._player2._sign = "rock";
   }
-  _ShowResult() {
+
+  _TryAgain() {
+    console.log("Try again");
+  }
+
+  _ShowResult(result) {
     console.log("Show Result");
+    console.log(result);
+    switch (result) {
+      case "win":
+        break;
+      case "lose":
+        break;
+      case "tie":
+        break;
+    }
   }
 }
