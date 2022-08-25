3e06876fbd99144c4a1ac34b34973e99d75d7be9
Lucas <lucaszago.ph@gmail.com>
2022-08-25 12:50:48 +0200

    TCW complete main without the UI

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 let app = null;
 let player1;
 let player2;
-
-document.addEventListener("DOMContentLoaded", () => {
+const actionBtns = [document.querySelector(".rock"), document.querySelector(".paper"), document.querySelector(".scissors")];
+function init() {
   player1 = new Player();
   player2 = new Player();
-  app = new Game(player1, player2);
-});
+  app = new Game();
+  app._Init(player1, player2);
+  actionBtns.forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      player1._ChooseSign(e.target.classList);
+      app._CheckResults();
+    })
+  );
+}
+document.addEventListener("DOMContentLoaded", init);
