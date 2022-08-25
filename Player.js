5bd9d3f57ec6d20c8305d0f5b879d864073342c7
Lucas <lucaszago.ph@gmail.com>
2022-08-25 12:48:07 +0200

    TCW complete player without UI

--- Player.js
+++ Player.js
@@ -1,16 +1,22 @@
 class Player {
-  constructor(player1, player2) {
+  constructor() {
     this._points = 0;
     this._sign = "rock";
-    this._Init();
   }
 
-  _Init() {
-    console.log("_Init Player");
-  }
-
-  _ChooseSign() {
-    console.log("Choose Sign");
+  _ChooseSign(e) {
+    if (e.contains("rock")) {
+      console.log("rock");
+      this._sign = "rock";
+    }
+    if (e.contains("paper")) {
+      console.log("paper");
+      this._sign = "paper";
+    }
+    if (e.contains("scissors")) {
+      console.log("scissors");
+      this._sign = "scissors";
+    }
   }
 
   _PointIncrease() {
@@ -20,5 +26,17 @@ class Player {
 
   _RandomizeSign() {
     console.log("Randomize Sign");
+    let rand = Math.floor(Math.random() * 3);
+    switch (rand) {
+      case 0:
+        this._sign = "rock";
+        break;
+      case 1:
+        this._sign = "paper";
+        break;
+      case 2:
+        this._sign = "scissors";
+        break;
+    }
   }
 }
